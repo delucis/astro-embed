53fa5d39422a5ae6180a5f9f407d8f6e1464164d
delucis <swithinbank@gmail.com>
2022-04-09 22:52:58 +0200

    Add basic test framework & runner

---
+++ tests/astro-embed-twitter.ts
@@ -0,0 +1,21 @@
+import { test } from 'uvu';
+import * as assert from 'uvu/assert';
+import { render, renderRaw } from './utils/render';
+
+test('it should render user information in the header', async () => {
+	const screen = await render('./packages/astro-embed-twitter/Tweet.astro', {
+		id: '1511750228428435457',
+	});
+	const username = screen.getByText('@astrodotbuild');
+	assert.is(username.getAttribute('href'), 'https://twitter.com/astrodotbuild');
+});
+
+test('it should render if passed a full URL', async () => {
+	const screen = await render('./packages/astro-embed-twitter/Tweet.astro', {
+		id: 'https://twitter.com/astrodotbuild/status/1402352777020395521',
+	});
+	const username = screen.getByText('@astrodotbuild');
+	assert.is(username.getAttribute('href'), 'https://twitter.com/astrodotbuild');
+});
+
+test.run();
---
+++ tests/utils/render.ts
@@ -0,0 +1,77 @@
+/* eslint-disable no-console */
+import { prettyDOM, queries, screen } from '@testing-library/dom';
+import { getComponentOutput } from 'astro-component-tester';
+import { JSDOM } from 'jsdom';
+import lzString from 'lz-string';
+const { compressToEncodedURIComponent } = lzString;
+
+/**
+ * Render an Astro component to an interface matching
+ * `@testing-library/dom`’s `screen` API.
+ * @param path Path to an Astro component to render (relative to monorepo root)
+ * @param props Any props to pass to the component
+ * @returns A `@testing-library/dom` “screen” containing the rendered component
+ */
+export const render = async (path: string, props: Record<string, unknown>) => {
+	const { raw } = await getComponentOutput(path, props);
+	return getScreen(new JSDOM(raw));
+};
+
+/**
+ * Create an interface matching `@testing-library/dom`’s `screen`, but without
+ * needing a global DOM environment.
+ */
+function getScreen(container: JSDOM): typeof screen {
+	const { document } = container.window;
+
+	const debug: typeof screen.debug = (
+		element: Parameters<typeof screen.debug>[0] = document,
+		maxLength?: Parameters<typeof screen.debug>[1],
+		options?: Parameters<typeof screen.debug>[2]
+	) =>
+		Array.isArray(element)
+			? element.forEach((el) => logDOM(el, maxLength, options))
+			: logDOM(element, maxLength, options);
+
+	const logTestingPlaygroundURL: typeof screen.logTestingPlaygroundURL = (
+		element = document.body
+	) => {
+		if (!element || !('innerHTML' in element)) {
+			console.log(`The element you're providing isn't a valid DOM element.`);
+			return;
+		}
+		if (!element.innerHTML) {
+			console.log(`The provided element doesn't have any children.`);
+			return;
+		}
+		console.log(
+			`Open this URL in your browser\n\n${getPlaygroundUrl(element.innerHTML)}`
+		);
+	};
+
+	const fakeScreen = { debug, logTestingPlaygroundURL } as typeof screen;
+	for (const [key, query] of Object.entries(queries)) {
+		fakeScreen[key] = (...args) =>
+			query(container.window.document as unknown as HTMLElement, ...args);
+	}
+	return fakeScreen;
+}
+
+// Below are utilities borrowed from Testing Library’s screen implementation.
+// See: https://github.com/testing-library/dom-testing-library/blob/main/src/screen.ts
+
+function unindent(string: string) {
+	return string.replace(/[ \t]*[\n][ \t]*/g, '\n');
+}
+
+function encode(value: string) {
+	return compressToEncodedURIComponent(unindent(value));
+}
+
+function getPlaygroundUrl(markup: string) {
+	return `https://testing-playground.com/#markup=${encode(markup)}`;
+}
+
+function logDOM(...args) {
+	console.log(prettyDOM(...args));
+}
