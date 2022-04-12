4dca30d6752359febaed0f01d2ca4e22a0a3dc34
delucis <swithinbank@gmail.com>
2022-04-12 21:55:51 +0200

    Refactor to separate URL matcher from component

---
+++ .changeset/spotty-beans-move.md
@@ -0,0 +1,5 @@
+---
+"@astro-community/astro-embed-twitter": patch
+---
+
+Refactor to separate URL matcher from component
---
+++ packages/astro-embed-twitter/matcher.js
@@ -0,0 +1,14 @@
+// Thanks to eleventy-plugin-embed-twitter
+// https://github.com/gfscott/eleventy-plugin-embed-twitter/blob/main/lib/extractMatch.js
+const urlPattern =
+	/(?=(\s*))\1(?:<a [^>]*?>)??(?=(\s*))\2(?:https?:)??(?:\/\/)??(?:w{3}\.)??(?:twitter\.com)\/([a-zA-Z0-9_]{1,15})?(?:\/(?:status)\/)(\d+)?(?:[^\s<>]*)(?=(\s*))\5(?:<\/a>)??(?=(\s*))\6/;
+
+/**
+ * Extract a Tweet ID from a URL if it matches the pattern.
+ * @param {string} url URL to test
+ * @returns {string|undefined} A Tweet ID or undefined if none matched
+ */
+export default function urlMatcher(url) {
+	const match = url.match(urlPattern);
+	return match?.[4];
+}
