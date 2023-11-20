b00cfabc1c4a433681f3c14bdc835ae25c89f1e4
vasfvitor <gitkey@virtuaires.com.br>
2023-11-20 14:52:51 -0300

    add demos + remark plugin

--- packages/astro-embed-spotify/matcher.ts
+++ packages/astro-embed-spotify/matcher.ts
@@ -1,14 +1,5 @@
 // Thanks to eleventy-plugin-embed-everything/spotify
 // https://github.com/gfscott/eleventy-plugin-embed-everything/blob/main/packages/spotify/lib/pattern.js
-/* The object returned by this regex is an array, with the following structure:
-* - [0]: The entire match
-* - [1]: Arbitrary whitespace
-* - [2]: Arbitrary whitespace
-* - [3]: Media type
-* - [4]: Media ID
-* - [5]: Arbitrary whitespace
-* - [6]: Arbitrary whitespace
-*/
 const urlPattern = /(?=(\s*))\1(?:<a [^>]*?>)??(?=(\s*))\2(?:https?:)??(?:\/\/)??(?:open\.|www\.)??spotify\.com\/(?:user)??\/??(?:[0-9a-zA-Z]+)??\/??(playlist|track|album|artist|episode|show)\/([0-9a-zA-Z]{22})(?:[^\s<>]*)(?=(\s*))\5(?:<\/a>)??(?=(\s*))\6/g;
 
 /**
