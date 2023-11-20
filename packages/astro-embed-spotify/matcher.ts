5b058e121e43f4bcb51ad0f189537449f16bf469
vasfvitor <gitkey@virtuaires.com.br>
2023-11-20 20:24:21 -0300

    Update matcher.ts

--- packages/astro-embed-spotify/matcher.ts
+++ packages/astro-embed-spotify/matcher.ts
@@ -4,9 +4,9 @@ const urlPattern =
 	/(?=(\s*))\1(?:<a [^>]*?>)??(?=(\s*))\2(?:https?:)??(?:\/\/)??(?:open\.|www\.)??spotify\.com\/(?:user)??\/??(?:[0-9a-zA-Z]+)??\/??(playlist|track|album|artist|episode|show)\/([0-9a-zA-Z]{22})(?:[^\s<>]*)(?=(\s*))\5(?:<\/a>)??(?=(\s*))\6/g;
 
 /**
- * Return a Track URL from a URL if it matches the pattern.
+ * Return a Track URL if it matches the pattern.
  * @param url URL to test
- * @returns A Track ID or undefined if none matched
+ * @returns A Track URL or undefined if none matched
  */
 export default function urlMatcher(url: string): string | undefined {
 	const match = url.match(urlPattern);
