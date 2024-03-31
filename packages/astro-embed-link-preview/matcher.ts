55dad8f4763489d107be85d1a69e07ba7d070b2f
Chris Swithinbank <swithinbank@gmail.com>
2024-03-31 23:41:56 +0200

    Cleanup comment

--- packages/astro-embed-link-preview/matcher.ts
+++ packages/astro-embed-link-preview/matcher.ts
@@ -1,7 +1,5 @@
-// matches any url, it does seems to be a issue if placed before others matchers in remark-plugin
-// as it is now (last one to match), seems to be ok
-// TODO: Check if possible to allow end user to set an allow list or exclusive domain
-const urlPattern = /(https?:\/\/\S+)/;
+// Matches any HTTPS URL.
+const urlPattern = /(https:\/\/\S+)/;
 
 export default function urlMatcher(url: string): string | undefined {
 	const match = url.match(urlPattern);
