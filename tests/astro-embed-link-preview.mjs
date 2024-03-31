1652aca245e5da77cf04983619a211b027dab36a
Chris Swithinbank <swithinbank@gmail.com>
2024-03-31 18:30:11 +0200

    Add test for video

--- tests/astro-embed-link-preview.mjs
+++ tests/astro-embed-link-preview.mjs
@@ -1,5 +1,6 @@
 import { test } from 'uvu';
-import { renderScreen } from './utils/render.mjs';
+import * as assert from 'uvu/assert';
+import { renderDOM, renderScreen } from './utils/render.mjs';
 
 test('it should render user meta data on card', async () => {
 	const screen = await renderScreen(
@@ -8,6 +9,18 @@ test('it should render user meta data on card', async () => {
 	);
 	screen.getByText('Welcome, World | Astro');
 	screen.getByText(/^Astro's new look is live!/);
+});
+
+test('it should render a video if available', async () => {
+	const { document } = await renderDOM(
+		'./packages/astro-embed-link-preview/LinkPreview.astro',
+		{ id: 'https://fosstodon.org/@mikeneu/112123823339364565' }
+	);
+	const embed = document.querySelector('.link-preview');
+	assert.ok(embed);
+	assert.ok(embed.classList.contains('link-preview--has-video'));
+	const video = embed.querySelector('video');
+	assert.ok(video);
 });
 
 test.run();
