e66e986acfea78b785c31b831f5691283d9920b4
Chris Swithinbank <swithinbank@gmail.com>
2024-03-31 04:06:33 +0200

    Refactor `parseOpenGraph()`

--- packages/astro-embed-og/lib.ts
+++ packages/astro-embed-og/lib.ts
@@ -1,31 +1,23 @@
 import { safeGetDOM } from '@astro-community/astro-embed-utils';
 
-interface OG {
-	title: string;
-	description: string;
-	image: string;
-	url: string;
-}
-
 /**
  * Parses a html page to return social heads contents based on source.
  * @param pageUrl to parse
- * @returns {Promise<OG | undefined>}
  */
-export async function parseOpenGraph(pageUrl: string): Promise<OG | undefined> {
-	const NONE = 'none';
+export async function parseOpenGraph(pageUrl: string) {
 	const html = await safeGetDOM(pageUrl);
-	if (!html) {
-		return;
-	}
+	if (!html) return;
+
 	const ogTitle = html.querySelector("meta[property='og:title']");
 	const ogDescription = html.querySelector("meta[property='og:description']");
 	const ogImage = html.querySelector("meta[property='og:image']");
+	const ogImageAlt = html.querySelector("meta[property='og:image:alt']");
 	const ogUrl = html.querySelector("meta[property='og:url']");
 	return {
-		title: ogTitle?.getAttribute('content') || NONE,
-		description: ogDescription?.getAttribute('content') || NONE,
-		image: ogImage?.getAttribute('content') || NONE,
-		url: ogUrl?.getAttribute('content') || NONE,
+		title: ogTitle?.getAttribute('content'),
+		description: ogDescription?.getAttribute('content'),
+		image: ogImage?.getAttribute('content'),
+		imageAlt: ogImageAlt?.getAttribute('content'),
+		url: ogUrl?.getAttribute('content'),
 	};
 }
