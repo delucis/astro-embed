1d95f1cf4941700cd4287f04ce48b81443feafec
SAS NEWS <109570410+sas-news@users.noreply.github.com>
2024-04-04 09:39:42 +0900

    Add image domains to demo/astro.config.mjs

--- demo/astro.config.mjs
+++ demo/astro.config.mjs
@@ -4,5 +4,8 @@ import embed from 'astro-embed/integration';
 
 // https://astro.build/config
 export default defineConfig({
+	image: {
+		domains: ['i.ytimg.com', 'i.vimeocdn.com', 'astro.build'],
+	},
 	integrations: [embed(), mdx()],
 });
