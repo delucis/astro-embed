94e6b08ed74935ba75a467c9c6075fcc73e232eb
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2024-08-26 15:21:55 +0200

    fix: enforce trailing digit in mastodon matcher

--- packages/astro-embed-mastodon/matcher.ts
+++ packages/astro-embed-mastodon/matcher.ts
@@ -1,7 +1,7 @@
 // Thanks to eleventy-plugin-embed-mastodon
 // https://github.com/inframanufaktur/eleventy-plugin-embed-mastodon/blob/f8c1687abf1f88a6351e53e38f91aff0c95a5cb2/.eleventy.js#L46-L47
 const urlPattern =
-	/(?:https:\/\/)?([\w\d-]*?.?[\w\d-]*.[a-z]*\/@[\w\d_]*(?:@[\w\d]*?.?[\w\d]*.[a-z]*)?\/)(\d*)/;
+	/(?:https:\/\/)?([\w\d-]*?.?[\w\d-]*.[a-z]*\/@[\w\d_]*(?:@[\w\d]*?.?[\w\d]*.[a-z]*)?\/)(\d+)/;
 
 /**
  * Return a Toot URL from a URL if it matches the pattern.
