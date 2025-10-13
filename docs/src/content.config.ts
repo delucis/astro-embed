bbbb86bf638a1a456f27e044d4a107191137b72c
Chris Swithinbank <swithinbank@gmail.com>
2025-10-14 01:46:30 +0200

    docs: Update to Astro v5 and Starlight v0.36

--- docs/src/content.config.ts
+++ docs/src/content.config.ts
@@ -1,8 +1,10 @@
 import { defineCollection, z } from 'astro:content';
+import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
 
 export const collections = {
 	docs: defineCollection({
+		loader: docsLoader(),
 		schema: docsSchema({
 			extend: z.object({
 				// Require a description for every page.
